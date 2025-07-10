741d6eeef1a3c062130c6d2a249b99eed1a286be
Nicolas Brichet <nicolas.brichet@quantstack.net>
2025-07-10 23:27:51 +0200

    Add notebook toolbar button

--- src/correxit/commands.ts
+++ src/correxit/commands.ts
@@ -117,6 +117,7 @@ export function addCommands(options: {
         const is = cell.is!;
         const workbook = active.workbook!;
 
+        let rubric: Correxit.Rubric<'unlocked'> | undefined;
         if (is === 'answerable') {
           const expected = await input.answer({
             title: trans.__('Add expected output'),
@@ -126,14 +127,14 @@ export function addCommands(options: {
             return;
           }
           const payload = [await digest(expected)];
-          return Correxit.add(workbook, { id, is, payload });
+          rubric = await Correxit.add(workbook, { id, is, payload });
         }
 
         if (is !== 'comparable' && is !== 'correctable') {
           return;
         }
         if (cell.reference) {
-          return Correxit.add(workbook, { ...cell, id, is });
+          rubric = await Correxit.add(workbook, { ...cell, id, is });
         }
         const selected = await input.cell(workbook);
         if (selected) {
@@ -141,8 +142,10 @@ export function addCommands(options: {
           const reference = Correxit.Workbook.Cell.id(selected, true);
           const original = find(widgets, ({ model }) => Cell.id(model) === id)!;
           await workbook.content.scrollToCell(original);
-          return Correxit.add(workbook, { ...cell, id, is, reference });
+          rubric = await Correxit.add(workbook, { ...cell, id, is, reference });
         }
+        commands.notifyCommandChanged(CommandIDs.correct);
+        return rubric;
       }
     }),
     commands.addCommand(CommandIDs.convert, {
@@ -161,6 +164,7 @@ export function addCommands(options: {
         if (key) {
           const rubric = await Correxit.convert(workbook, key);
           await workbook.context.save();
+          commands.notifyCommandChanged(CommandIDs.correct);
           return rubric;
         }
       }
@@ -205,6 +209,7 @@ export function addCommands(options: {
           const workbook = active.workbook!;
           const rubric = await Correxit.lock(workbook);
           await workbook.context.save();
+          commands.notifyCommandChanged(CommandIDs.correct);
           return rubric;
         } catch (error) {
           void showErrorMessage(trans.__('Could not lock'), error as Error);
@@ -217,7 +222,9 @@ export function addCommands(options: {
       label: trans.__('Reset expected cell output'),
       execute: async (cell: CellCommandArgs) => {
         if (validate[CommandIDs.remove](cell)) {
-          return Correxit.remove(active.workbook!, cell.id!);
+          const rubric = await Correxit.remove(active.workbook!, cell.id!);
+          commands.notifyCommandChanged(CommandIDs.correct);
+          return rubric;
         }
       }
     }),
@@ -238,6 +245,7 @@ export function addCommands(options: {
         if (button.accept) {
           await Correxit.reset(workbook);
           await workbook.context.save();
+          commands.notifyCommandChanged(CommandIDs.correct);
         }
       }
     }),
@@ -256,7 +264,9 @@ export function addCommands(options: {
       },
       execute: async (cell: CellCommandArgs) => {
         if (validate[CommandIDs.toggle](cell)) {
-          return Correxit.toggle(active.workbook!, cell.id!);
+          const rubric = await Correxit.toggle(active.workbook!, cell.id!);
+          commands.notifyCommandChanged(CommandIDs.correct);
+          return rubric;
         }
       }
     }),
@@ -290,6 +300,7 @@ The command invokes an error message dialog if unlock fails.
           }
           const rubric = await Correxit.unlock(workbook, key);
           await workbook.context.save();
+          commands.notifyCommandChanged(CommandIDs.correct);
           return rubric;
         } catch (error) {
           const file = PathExt.basename(workbook.context.path);
--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -44,6 +44,7 @@ export class Sidebar extends ReactWidget {
   }
 
   protected ping() {
+    this.commands.notifyCommandChanged(Correxit.CommandIDs.correct);
     this.pinged.emit(undefined);
   }
 
