f3613407d435d079f833f2ccbd082b0ac776dbc5
Afshin T. Darian <git@darian.link>
2025-07-12 14:09:11 +0100

    Remove workbook ID from the UI

--- src/sidebar/header.tsx
+++ src/sidebar/header.tsx
@@ -18,7 +18,6 @@ export const Header: React.FC<{
     <section className="correxit-header">
       <File trans={trans} workbook={workbook} />
       <CommandToolbarButtonComponent commands={commands} id={convert} />
-      <ID trans={trans} workbook={workbook} />
       <CommandToolbarButtonComponent commands={commands} id={lock} />
       <CommandToolbarButtonComponent commands={commands} id={unlock} />
       <CommandToolbarButtonComponent commands={commands} id={correct} />
@@ -45,23 +44,5 @@ const File: React.FC<{
         </>
       )}
     </UseSignal>
-  );
-};
-
-const ID: React.FC<{
-  trans: IRenderMime.TranslationBundle;
-  workbook: Correxit.Workbook;
-}> = ({ trans, workbook }) => {
-  const id = workbook.content.model?.getMetadata('correxit')?.id;
-  if (!id) {
-    return <></>;
-  }
-  return (
-    <>
-      <h4>{trans.__('Workbook ID:')}</h4>
-      <div className="correxit-monospace" title={id}>
-        {id}
-      </div>
-    </>
   );
 };
