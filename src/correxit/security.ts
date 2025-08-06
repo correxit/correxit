c6ad71563311ba39a9e32d0c009ff77e240e825d
Afshin T. Darian <git@darian.link>
2025-08-06 14:46:31 +0100

    add return types to security functions

--- src/correxit/security.ts
+++ src/correxit/security.ts
@@ -3,7 +3,7 @@ import * as pgp from 'openpgp';
 const SALT = await digest('correxit:salt');
 const PEPPER = await digest('correxit:pepper');
 
-export async function decrypt(text: string, password: string) {
+export async function decrypt(text: string, password: string): Promise<string> {
   let message;
   try {
     message = await pgp.readMessage({ armoredMessage: text });
@@ -13,18 +13,22 @@ export async function decrypt(text: string, password: string) {
   return (await pgp.decrypt({ message, passwords: [password] })).data;
 }
 
-export async function digest(text: string) {
+export async function digest(text: string): Promise<string> {
   const encoded = new TextEncoder().encode(text);
   const hash = await crypto.subtle.digest('SHA-256', encoded);
   const hexadecimal = (digit: number) => digit.toString(16).padStart(2, '0');
   return Array.from(new Uint8Array(hash)).map(hexadecimal).join('');
 }
 
-export async function encrypt(text: string, password: string) {
+export async function encrypt(text: string, password: string): Promise<string> {
   const message = await pgp.createMessage({ text });
   return pgp.encrypt({ message, passwords: [password] });
 }
 
-export async function keygen(passphrase: string, salt = SALT, pepper = PEPPER) {
+export async function keygen(
+  passphrase: string,
+  salt = SALT,
+  pepper = PEPPER
+): Promise<string> {
   return digest(`${salt}:${await digest(passphrase)}:${pepper}`);
 }
