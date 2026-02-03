f5b66236378bf2b74f777b6e5c3e894427e247d7
A. T. Darian <git@darian.at>
2026-02-03 22:38:35 +0000

    only export to window if in galata environment

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Correxit, Rubric, Workbook } from './correxit';
 export { Correxit, Rubric, Workbook };
 export default [consumer, corrector, source, ui, unlocker];
 
-if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined' && !!(window as any).galata) {
   Object.defineProperty(window as any, '__correxit__', {
     configurable: false,
     enumerable: false,
