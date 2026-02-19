cd268611ea4e6565127771efb9d4fc782f9e3a6c
A. T. Darian <git@darian.at>
2026-02-19 20:35:18 +0000

    defensive CI playwright config

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -8,6 +8,13 @@ module.exports = {
   // All spec files share one Jupyter server and its file system, so tests
   // must run sequentially to avoid cross-file interference.
   workers: 1,
+  retries: process.env.CI ? 2 : 0,
+  timeout: 120 * 1000,
+  use: {
+    ...baseConfig.use,
+    actionTimeout: 30 * 1000,
+    navigationTimeout: 30 * 1000
+  },
   reporter: [['list'], ['html', { open: 'never' }]],
   webServer: {
     command: 'jlpm start',
