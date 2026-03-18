2a33796a4a6b16f3f5aa104692ff36ba7f14b78c
A. T. Darian <git@darian.at>
2026-03-18 01:31:13 +0000

    fix tests

--- src/__tests__/mocks/security.ts
+++ src/__tests__/mocks/security.ts
@@ -12,6 +12,10 @@ export const decrypt = jest.fn(async (text: string, key: string) => {
 
 export const digest = jest.fn(async (text: string) => `DIGEST<${text}>`);
 
+export const hmac = jest.fn(
+  async (message: string, key: string) => `HMAC<${message}:${key}>`
+);
+
 export const keygen = jest.fn(
   async (passphrase: string, salt: string) =>
     `KEY<${passphrase}:${salt || 'default'}>`
